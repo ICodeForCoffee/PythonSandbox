22598730dba1fee00bcff5cf8f888299c68ca055
Matthew Pouliotte <poul102@gmail.com>
2025-01-03 11:52:02 -0500

    I claned up the code a little in RollWithAdvantage.py.

--- RollWithAdvantage.py
+++ RollWithAdvantage.py
@@ -7,27 +7,26 @@ def main():
     times_to_test = 1000000
     times_advantage_helped = 0
     improvement_total = 0
-    silly_hundred = 100
     
-    for count in range(times_to_test):
+    for x in range(times_to_test):
         advantage_helped, improvement = calculate_advantage()
         if (advantage_helped):
             times_advantage_helped += 1
             improvement_total += improvement
     
-    precent_rolls_improved = 0
-    advantaged_improved = 0
-    advantaged_improved_rolls_it_helped_by = 0
-    
     if times_advantage_helped: 
         precent_rolls_improved = times_advantage_helped / times_to_test * 100
-        advantaged_improved = improvement_total/times_to_test
-        advantaged_improved_rolls_it_helped_by = improvement_total/times_advantage_helped
+        advantage_improved_by = improvement_total/times_to_test
+        advantage_improved_rolls_it_helped_by = improvement_total/times_advantage_helped
+    else:
+        precent_rolls_improved = 0
+        advantage_improved_by = 0
+        advantage_improved_rolls_it_helped_by = 0
     
     print(f"We rolled for advantage {times_to_test:,} times\n")
     print(f"Advantaged improved our results {precent_rolls_improved:.2f}% of the time")
-    print(f"Across all rolls we saw a +{advantaged_improved:.2f} to rolls")
-    print(f"Across rolls were advantage helped we saw a +{advantaged_improved_rolls_it_helped_by:.2f} to rolls")
+    print(f"Across all rolls we saw a +{advantage_improved_by:.2f} to rolls")
+    print(f"Across rolls were advantage helped we saw a +{advantage_improved_rolls_it_helped_by:.2f} to rolls")
     
 def calculate_advantage():
     roll1 = np.random.randint(1, 21)
