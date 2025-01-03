638d9e7009bdaca49d54b08bc2ca88291dae613f
Matthew Pouliotte <poul102@gmail.com>
2025-01-03 13:19:40 -0500

    Fixed some bugs I had since I committed too soon.

--- RollWithAdvantage2.py
+++ RollWithAdvantage2.py
@@ -7,12 +7,12 @@ def main():
     dice_results = {}
     dice_improvement = {}
     
-    #Let's get our results and then put them in.
+    # Let's get our results and then put them in.
     for roll in range(1, 21):
         dice_results[roll] = 0
         
     for roll in range(0, 20):
-        dice_improvement[roll]
+        dice_improvement[roll] = 0
 
     for dice1 in range(1, 21):
         for dice2 in range(1, 21):
@@ -20,6 +20,7 @@ def main():
             difference = abs(dice1 - dice2)
             
             dice_results[result] += 1
+            dice_improvement[difference] += 1
             
     display_roll_results(dice_results)
     display_roll_improvement(dice_improvement)
@@ -39,18 +40,18 @@ def display_roll_results(dice_results):
     plt.title("Possible results for rolling two d20s")
     plt.show()
     
-def display_roll_improvement(dice_results):
+def display_roll_improvement(dice_improvement):
     xaxis = np.array([])
     yaxis = np.array([])
     
     for roll in range(0, 20):
         xaxis = np.append(xaxis, roll)
-        yaxis = np.append(yaxis, dice_results[roll])
+        yaxis = np.append(yaxis, dice_improvement[roll])
     
     plt.bar(xaxis, yaxis)
     plt.xlabel("Imprement Result")
     plt.xlabel("Occurrence")
-    plt.title("If we roll dice concurrently, we should see the folling improvement")
+    plt.title("If we roll dice concurrently, we should see the following improvement between the two dice")
     plt.show()
 
 main()
