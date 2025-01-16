665366d8b83cd8a4ea68bf850b7bd5e79c8a775b
Matthew Pouliotte <poul102@gmail.com>
2025-01-15 23:36:47 -0500

    Added another check for solving.

--- Sudoku.py
+++ Sudoku.py
@@ -39,6 +39,7 @@ def solve_puzzle(puzzle):
         changesd_squares = 0
         puzzle = populate_possible_values(puzzle)
         puzzle = prune_possibilities(puzzle)
+        puzzle = perform_analysis(puzzle)
         puzzle, changesd_squares = promote_solved_squares(puzzle)
     
     print("\nAfter attempting to solve\n")
@@ -91,10 +92,11 @@ def populate_possible_values(puzzle):
 def prune_possibilities(puzzle):
     for x in range(9):
         for y in range(9):
-            found_match = False
+            found_axis_requirement = False
+            found_box_requirement = False
             if puzzle.squares[x][y]['value'] == " ":
                 for possible_value in puzzle.squares[x][y]['possible_values']:
-                    if not found_match:
+                    if not found_axis_requirement and not found_box_requirement:
                         only_x_axis_appearance = True
                         only_y_axis_appearance = True
                         
@@ -107,8 +109,44 @@ def prune_possibilities(puzzle):
                                 only_y_axis_appearance = False
                         
                         if only_x_axis_appearance or only_y_axis_appearance:
-                            found_match = True
+                            found_axis_requirement = True
                             puzzle.squares[x][y]['possible_values'] = [possible_value]
+                        else:
+                            only_box_appearance = True
+                            #Check the box
+                            if 0 <= x <= 2:
+                                xrange = [0, 1, 2]
+                            elif 3 <= x <= 5:
+                                xrange = [3, 4, 5]
+                            else:
+                                xrange = [6, 7, 8]
+                                
+                            if 0 <= y <= 2:
+                                yrange = [0, 1, 2]
+                            elif 3 <= y <= 5:
+                                yrange = [3, 4, 5]
+                            else:
+                                yrange = [6, 7, 8]
+                                
+                            for xaxis in xrange:
+                                for yaxis in yrange:
+                                    if not (xaxis == x and yaxis == y):
+                                        if possible_value in puzzle.squares[xaxis][yaxis]['possible_values']:
+                                            only_box_appearance = False
+                                            
+                            if only_box_appearance:
+                                found_box_requirement = True
+                                puzzle.squares[x][y]['possible_values'] = [possible_value]
+    return puzzle
+
+def perform_analysis(puzzle):
+    # Check if value must appear in the row for possibilities.
+    for x in range(9):
+        for y in range(9):
+            found_match = False
+            if puzzle.squares[x][y]['value'] == " ":
+                for possible_value in puzzle.squares[x][y]['possible_values']:
+                    pass
     
     return puzzle
 
