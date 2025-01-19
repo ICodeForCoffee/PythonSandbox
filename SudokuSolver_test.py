a2a7608caa91298399b36c7d5c88fd6346ab269a
Matthew Pouliotte <poul102@gmail.com>
2025-01-19 15:47:05 -0500

    Adding another test for solving.

--- SudokuSolver_test.py
+++ SudokuSolver_test.py
@@ -26,3 +26,45 @@ def test_basic_solving():
     puzzle = SudokuSolver.solve_puzzle(puzzle)
     assert puzzle.squares[3][4]['value'] == 6
     assert puzzle.is_solved() == True
+
+    matrix = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, ' ', 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ]
+    
+    for x in range(9):
+        for y in range(9):
+            puzzle.squares[x][y]['value'] = matrix[x][y]
+
+def test_simple_solving():
+    puzzle = SudokuPuzzle()
+    
+    #This is the same Matrix from sudoku-puzzle1.txt    
+    matrix = [
+        [' ',' ', 2 , 7 ,' ',' ', 1 , 5 , 6 ],
+        [' ', 3 ,' ', 6 , 2 , 8 ,' ', 4 ,' '],
+        [ 7 , 6 , 9 ,' ',' ',' ',' ', 3 ,' '],
+        [ 9 ,' ', 7 ,' ', 4 , 5 , 3 ,' ',' '],
+        [' ',' ',' ',' ', 8 ,' ', 7 , 2 ,' '],
+        [ 6 , 2 ,' ', 3 ,' ',' ', 5 ,' ',' '],
+        [' ', 5 ,' ', 1 ,' ', 3 ,' ',' ', 9 ],
+        [' ', 7 ,' ',' ', 9 ,' ', 2 ,' ', 5 ],
+        [ 4 ,' ', 1 , 5 , 7 ,' ',' ',' ',' ']
+    ]
+    
+    for x in range(9):
+        for y in range(9):
+            puzzle.squares[x][y]['value'] = matrix[x][y]
+
+    
+    assert puzzle.is_solved() == False
+    puzzle = SudokuSolver.solve_puzzle(puzzle)
+    assert puzzle.is_solved() == True
+    assert puzzle.guessing_used == False
