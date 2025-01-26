c9f4e5c7021225d0a5496e44eb5081ed807c9f82
Matthew Pouliotte <poul102@gmail.com>
2025-01-26 10:06:05 -0500

    Added timing of solve_puzzle() method.

--- Sudoku.py
+++ Sudoku.py
@@ -1,5 +1,6 @@
 from SudokuSolver import SudokuSolver
 import argparse
+import time
 
 def main():
     parser = argparse.ArgumentParser(prog="Sudoku Solver", description="Sudoku Solver")
@@ -11,15 +12,26 @@ def main():
     
     print("Initial puzzle\n")
     instance.display_puzzle(puzzle)
+    
+    start_time = time.time()
     puzzle = instance.solve_puzzle(puzzle)
+    end_time = time.time()
+    
     print("After attempting to solve\n")
     instance.display_puzzle(puzzle)
+    
+    
+    
     print(f"This puzzle is {"solved" if puzzle.is_solved() == True else "unsolved"}")
     if __debug__:
+        print()
         if puzzle.guessing_used == True:
             print(f"Guessing was used to calculate this result")
         if puzzle.analysis_helped == True:
             print("The analysis method helped")
+        time_elapsed = end_time - start_time
+        print()
+        print(f"The solving method took {time_elapsed:.5f} seconds.")
     print()
 
 main()
